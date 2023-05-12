09ed4d7acb85497fd2f130c5a5038d12eab59ed7
Wond-Z <wond-z@live.com>
2023-05-12 10:54:11 +0800

    添加服务器ip访问提示

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var os = require('os');
 
 app.use(express.static(path.join(__dirname, 'web')));
 
@@ -19,6 +20,23 @@ app.all('*', function (req, res, next) {
     next();
 });
 
+// 获取内网ip
+function getIPAddress() {
+    let IPAddress = '';
+    // interfaces的结果会是当前主机的所有网络信息（包含本地127.0.0.1、虚拟机ip、主机真实ip）
+    var interfaces = os.networkInterfaces();
+    for (var devName in interfaces) {
+        var iface = interfaces[devName];
+        for (var i = 0; i < iface.length; i++) {
+            var alias = iface[i];
+            if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
+                IPAddress = alias.address;
+            }
+        }
+    }
+    return IPAddress;
+};
+
 
 var server = app.listen(6868, '0.0.0.0', function () {
     var host = server.address().address;
@@ -26,4 +44,5 @@ var server = app.listen(6868, '0.0.0.0', function () {
 
     // console.log('服务启动，访问地址为 http://%s:%s', host, port);
     console.log('服务启动，访问地址为 http://%s:%s', 'localhost', port);
+    console.log('服务启动，访问地址为 http://%s:%s', getIPAddress(), port);
 });
